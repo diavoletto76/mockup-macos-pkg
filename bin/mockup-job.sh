e2e24cfb8ea3597f971643c323afc6816de2bf4f
diavoletto76 <diavoletto76@gmail.com>
2021-05-30 09:47:19 +0200

    Improves mockup-job

--- bin/mockup-job.sh
+++ bin/mockup-job.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-echo `date "+%Y-%m-%d %H:%M:%S"` "Hello World!"
+echo '---'
+date "+%Y-%m-%d %H:%M:%S"
+echo 'Hello World!'
+echo "Arg1: $1"
+echo "Arg2: $2"
+echo "Arg3: $3"
+
+# sleep 70
